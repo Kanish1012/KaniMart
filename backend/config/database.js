632ab898a62a83de24cf0f55d3651a7fcd59feaa
Kanish <kanishv1012@gmail.com>
2024-09-01 21:07:06 +0530

    setup config and db

---
+++ backend/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+    mongoose
+        .connect(process.env.DB_LOCAL_URI)
+        .then((con) => {
+            console.log(
+                `MongoDB is connect to the host: ${con.connection.host}`
+            );
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+module.exports = connectDatabase;
