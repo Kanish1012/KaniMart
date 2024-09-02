a0bbb4c63406bba0c2f3f8902f93ef1ae0509882
Kanish <kanishv1012@gmail.com>
2024-09-02 16:57:09 +0530

    create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,17 @@
+const Product = require("../models/productModel");
+
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
         message: "This route will show all the products in database",
+    });
+};
+
+//Create Product - /api/v1/products/new
+exports.newProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product,
     });
 };
