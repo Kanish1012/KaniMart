6cc0563840b95fca9b9adae0e5acd735e101ad21
Kanish <kanishv1012@gmail.com>
2024-09-03 15:56:45 +0530

    Update Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,14 @@
 const Product = require("../models/productModel");
 
+//Create Product - /api/v1/products/new
+exports.newProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product,
+    });
+};
+
 //Get Products - /api/v1/products
 exports.getProducts = async (req, res, next) => {
     const products = await Product.find();
@@ -10,7 +19,7 @@ exports.getProducts = async (req, res, next) => {
     });
 };
 
-//Get Single Product -
+//Get Single Product - /api/v1/product/:id
 exports.getSingleProduct = async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
@@ -27,10 +36,23 @@ exports.getSingleProduct = async (req, res, next) => {
     });
 };
 
-//Create Product - /api/v1/products/new
-exports.newProduct = async (req, res, next) => {
-    const product = await Product.create(req.body);
-    res.status(201).json({
+//Update Product - /api/v1/product/:id
+exports.updataProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found",
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
         success: true,
         product,
     });
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,11 +3,12 @@ const {
     getProducts,
     newProduct,
     getSingleProduct,
+    updataProduct,
 } = require("../controllers/productController");
 const router = express.Router();
 
-router.route("/products").get(getProducts);
-router.route("/product/:id").get(getSingleProduct);
 router.route("/products/new").post(newProduct);
+router.route("/products").get(getProducts);
+router.route("/product/:id").get(getSingleProduct).put(updataProduct);
 
 module.exports = router;
