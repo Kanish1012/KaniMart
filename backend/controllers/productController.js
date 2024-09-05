73d612533cd16df27757f4bb4d76fa9dbbd07bc0
Kanish <kanishv1012@gmail.com>
2024-09-05 19:31:51 +0530

    Search Products by Keywords

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncError");
+const APIFeatures = require("../utils/apiFeatures");
 
 //Create Product - /api/v1/products/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,7 +14,9 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get Products - /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-    const products = await Product.find();
+    const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+
+    const products = await apiFeatures.query;
     res.status(200).json({
         success: true,
         count: products.length,
