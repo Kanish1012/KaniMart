05a6dac5ee7f24fdd3b518c34fd6191d78faf6e7
Kanish <kanishv1012@gmail.com>
2024-09-05 19:53:55 +0530

    Filter Category

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,7 +14,9 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get Products - /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-    const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+        .filter();
 
     const products = await apiFeatures.query;
     res.status(200).json({
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -18,6 +18,24 @@ class APIFeatures {
         this.query.find({ ...keyword });
         return this;
     }
+
+    filter() {
+        const queryStrCopy = { ...this.queryStr };
+
+        //Removing fields from query
+        const removeFields = ["keyword", "limit", "page"];
+        removeFields.forEach((field) => queryStrCopy[field]);
+
+        if (queryStrCopy.category) {
+            queryStrCopy.category = {
+                $regex: queryStrCopy.category,
+                $options: "i",
+            };
+        }
+
+        this.query.find(queryStrCopy);
+        return this;
+    }
 }
 
 module.exports = APIFeatures;
