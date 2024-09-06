0d32d6f9999ad0ea25a5b418a0d93f191895956e
Kanish <kanishv1012@gmail.com>
2024-09-06 22:15:35 +0530

    Sending Token in Cookie

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -2,7 +2,15 @@ const sendToken = (user, statusCode, res) => {
     //Creating JWT Token
     const token = user.getJwtToken();
 
-    res.status(statusCode).json({
+    //Setting cookies in ms
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+    };
+
+    res.status(statusCode).cookie("token", token, options).json({
         success: true,
         token,
         user,
