c1abceedfbe95375712b0cf3a2b8e447d1672a15
Kanish <kanishv1012@gmail.com>
2024-09-08 02:11:38 +0530

    Creating Home Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
+import Home from "./components/Home";
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Home />
             <Footer />
         </div>
     );
