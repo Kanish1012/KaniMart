a2a9af7588c410defc7c11dcf34baba274732f32
Kanish <kanishv1012@gmail.com>
2024-09-07 20:39:53 +0530

    Header and Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
 
 function App() {
     return (
         <div className="App">
-            <h1>KaniMart</h1>
+            <Header />
+            <Footer />
         </div>
     );
 }
