41b0a8a424c8cabdb5ffa915a12a8567ba6f9e06
Kanish <kanishv1012@gmail.com>
2024-09-07 12:00:29 +0530

    Error Handling User API services

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -26,6 +26,21 @@ module.exports = (err, req, res, next) => {
             error = new Error(message);
         }
 
+        if (err.code == 11000) {
+            message = `Duplicate ${Object.keys(err.keyValue)} error`;
+            error = new Error(message);
+        }
+
+        if (err.name == "JSONWebTokenError") {
+            message = "JSON Web Token is invalid, Try again";
+            error = new Error(message);
+        }
+        
+        if (err.name == "TokenExpiredError") {
+            message = "JSON Web Token is expired, Try again";
+            error = new Error(message);
+        }
+
         res.status(err.statusCode).json({
             success: false,
             message: error.message || "Internal Server Error",
