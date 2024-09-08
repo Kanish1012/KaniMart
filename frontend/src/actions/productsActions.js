5fe586eaa81939776331c133f27a8c1c14dd7d1f
Kanish <kanishv1012@gmail.com>
2024-09-08 13:20:52 +0530

    Creating Products Actions

---
+++ frontend/src/actions/productsActions.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import {
+    productsFail,
+    productsRequest,
+    productsSuccess,
+} from "../slices/productsSlice";
+
+export const getProducts = async (dispatch) => {
+    try {
+        dispatch(productsRequest());
+        const { data } = await axios.get("/api/v1/products");
+        dispatch(productsSuccess(data));
+    } catch (error) {
+        dispatch(productsFail(error.response.data.message));
+    }
+};
