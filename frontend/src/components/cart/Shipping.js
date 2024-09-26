2ec07a33287f03c420ad5259ef483490dd551ce5
Kanish <kanishv1012@gmail.com>
2024-09-27 00:22:24 +0530

    Checkout Steps

--- frontend/src/components/cart/Shipping.js
+++ frontend/src/components/cart/Shipping.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { countries } from "countries-list";
 import { saveShippingInfo } from "../../slices/cartSlice";
 import { useNavigate } from "react-router-dom";
+import CheckoutSteps from "./CheckoutSteps";
 
 export default function Shipping() {
     const { shippingInfo } = useSelector((state) => state.cartState);
@@ -33,96 +34,101 @@ export default function Shipping() {
     };
 
     return (
-        <div className="row wrapper">
-            <div className="col-10 col-lg-5">
-                <form onSubmit={submitHandler} className="shadow-lg">
-                    <h1 className="mb-4">Shipping Info</h1>
-                    <div className="form-group">
-                        <label htmlFor="address_field">Address</label>
-                        <input
-                            type="text"
-                            id="address_field"
-                            className="form-control"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                            required
-                        />
-                    </div>
+        <Fragment>
+            <CheckoutSteps shipping />
+            <div className="row wrapper">
+                <div className="col-10 col-lg-5">
+                    <form onSubmit={submitHandler} className="shadow-lg">
+                        <h1 className="mb-4">Shipping Info</h1>
+                        <div className="form-group">
+                            <label htmlFor="address_field">Address</label>
+                            <input
+                                type="text"
+                                id="address_field"
+                                className="form-control"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                                required
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="city_field">City</label>
-                        <input
-                            type="text"
-                            id="city_field"
-                            className="form-control"
-                            value={city}
-                            onChange={(e) => setCity(e.target.value)}
-                            required
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="city_field">City</label>
+                            <input
+                                type="text"
+                                id="city_field"
+                                className="form-control"
+                                value={city}
+                                onChange={(e) => setCity(e.target.value)}
+                                required
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="state_field">State</label>
-                        <input
-                            type="text"
-                            id="state_field"
-                            className="form-control"
-                            value={state}
-                            onChange={(e) => setState(e.target.value)}
-                            required
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="state_field">State</label>
+                            <input
+                                type="text"
+                                id="state_field"
+                                className="form-control"
+                                value={state}
+                                onChange={(e) => setState(e.target.value)}
+                                required
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="postal_code_field">Postal Code</label>
-                        <input
-                            type="number"
-                            id="postal_code_field"
-                            className="form-control"
-                            value={postalCode}
-                            onChange={(e) => setPostalCode(e.target.value)}
-                            required
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="postal_code_field">
+                                Postal Code
+                            </label>
+                            <input
+                                type="number"
+                                id="postal_code_field"
+                                className="form-control"
+                                value={postalCode}
+                                onChange={(e) => setPostalCode(e.target.value)}
+                                required
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="phone_field">Phone No</label>
-                        <input
-                            type="phone"
-                            id="phone_field"
-                            className="form-control"
-                            value={phoneNo}
-                            onChange={(e) => setPhoneNo(e.target.value)}
-                            required
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="phone_field">Phone No</label>
+                            <input
+                                type="phone"
+                                id="phone_field"
+                                className="form-control"
+                                value={phoneNo}
+                                onChange={(e) => setPhoneNo(e.target.value)}
+                                required
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="country_field">Country</label>
-                        <select
-                            id="country_field"
-                            className="form-control"
-                            value={country}
-                            onChange={(e) => setCountry(e.target.value)}
-                            required
+                        <div className="form-group">
+                            <label htmlFor="country_field">Country</label>
+                            <select
+                                id="country_field"
+                                className="form-control"
+                                value={country}
+                                onChange={(e) => setCountry(e.target.value)}
+                                required
+                            >
+                                {countryList.map((country, i) => (
+                                    <option key={i} value={country.name}>
+                                        {country.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <button
+                            type="submit"
+                            id="shipping_btn"
+                            className="btn btn-block py-3"
                         >
-                            {countryList.map((country, i) => (
-                                <option key={i} value={country.name}>
-                                    {country.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <button
-                        type="submit"
-                        id="shipping_btn"
-                        className="btn btn-block py-3"
-                    >
-                        CONTINUE
-                    </button>
-                </form>
+                            CONTINUE
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </Fragment>
     );
 }
