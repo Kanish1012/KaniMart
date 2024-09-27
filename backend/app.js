1f9def01cdea9795c95a3e84b1cd5f9f08b7909d
Kanish <kanishv1012@gmail.com>
2024-09-27 19:55:08 +0530

    Backend Stripe Integration

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const app = express();
 const errorMiddleware = require("./middlewares/error");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: path.join(__dirname, "./config/config.env") });
 
 //Middleware
 app.use(express.json());
@@ -13,11 +16,13 @@ const products = require("./routes/product");
 const auth = require("./routes/auth");
 const order = require("./routes/order");
 const error = require("./middlewares/error");
+const payment = require("./routes/payment");
 
 //Middleware
 app.use("/api/v1/", products);
 app.use("/api/v1", auth);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 app.use(errorMiddleware);
 
 module.exports = app;
---
+++ backend/routes/payment.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { isAuthenticatedUser } = require("../middlewares/authenticate");
+const {
+    processPayment,
+    sendStripeAPI,
+} = require("../controllers/paymentController");
+
+router.route("/payment/process").post(isAuthenticatedUser, processPayment);
+router.route("/stripeapi").get(isAuthenticatedUser, sendStripeAPI);
+
+module.exports = router;
