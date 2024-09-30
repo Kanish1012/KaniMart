f01d7867397ee064bff385a69eb7dafd9a148076
Kanish <kanishv1012@gmail.com>
2024-09-30 23:11:45 +0530

    Revert "order Reducer & action"
    This reverts commit 43dc950f7779434b62675cd7ab4508ed691f8d17.

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -7,6 +7,6 @@ const {
 } = require("../controllers/paymentController");
 
 router.route("/payment/process").post(isAuthenticatedUser, processPayment);
-router.route("/stripeapi").get(sendStripeAPI);
+router.route("/stripeapi").get(isAuthenticatedUser, sendStripeAPI);
 
 module.exports = router;
